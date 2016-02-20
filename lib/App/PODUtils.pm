a297ae52ef029a0d6ce916c21a7fc2abb7bd77fe
perlancar (@pc-home) <perlancar@gmail.com>
2016-02-21 00:44:07 +0700

    Initial commit

---
+++ lib/App/PODUtils.pm
@@ -0,0 +1,16 @@
+package App::PODUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Command-line utilities related to POD
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities related to Perl
+POD:
+
+#INSERT_EXECS_LIST
+
+=cut
