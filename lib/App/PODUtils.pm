1fdc595c2773961a2c744c848b0841fb397393b0
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-06-03 05:44:53 +0700

    Add utility: podless

--- lib/App/PODUtils.pm
+++ lib/App/PODUtils.pm
@@ -3,6 +3,35 @@ package App::PODUtils;
 # DATE
 # VERSION
 
+use 5.010001;
+use strict;
+use warnings;
+
+our $arg_pod_multiple = {
+    schema => ['array*' => of=>'perl::podname*', min_len=>1],
+    req    => 1,
+    pos    => 0,
+    greedy => 1,
+    element_completion => sub {
+        require Complete::Module;
+        my %args = @_;
+        Complete::Module::complete_module(
+            find_pm=>0, find_pmc=>0, find_pod=>1, word=>$args{word});
+    },
+};
+
+our $arg_pod_single = {
+    schema => 'perl::podname*',
+    req    => 1,
+    pos    => 0,
+    completion => sub {
+        require Complete::Module;
+        my %args = @_;
+        Complete::Module::complete_module(
+            find_pm=>0, find_pmc=>0, find_pod=>1, word=>$args{word});
+    },
+};
+
 1;
 # ABSTRACT: Command-line utilities related to POD
 
