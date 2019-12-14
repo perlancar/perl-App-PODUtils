cc1b1ebf44d85a24aec316d70b830afa3d8eeabb
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2019-12-14 13:01:05 +0700

    Add utility: dump-pod-structure (poddump)

--- lib/App/PODUtils.pm
+++ lib/App/PODUtils.pm
@@ -1,12 +1,19 @@
 package App::PODUtils;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
 
 use 5.010001;
 use strict;
 use warnings;
 
+use File::Slurper::Dash 'read_text';
+
+our %SPEC;
+
+# old
 our $arg_pod_multiple = {
     schema => ['array*' => of=>'perl::podname*', min_len=>1],
     req    => 1,
@@ -20,6 +27,7 @@ our $arg_pod_multiple = {
     },
 };
 
+# old
 our $arg_pod_single = {
     schema => 'perl::podname*',
     req    => 1,
@@ -31,6 +39,70 @@ our $arg_pod_single = {
             find_pm=>0, find_pmc=>0, find_pod=>1, word=>$args{word});
     },
 };
+
+our %arg0_pod = (
+    pod => {
+        summary => 'Path to a .POD file, or a POD name (e.g. Foo::Bar) '.
+            'which will be searched in @INC',
+        description => <<'_',
+
+"-" means standard input.
+
+_
+        schema => 'perl::pod_filename*',
+        default => '-',
+        pos => 0,
+    },
+);
+
+our %arg0_pods = (
+    pods => {
+        'x.name.is_plural' => 1,
+        'x.name.singular' => 'pod',
+        summary => 'Paths to .POD files, or POD names (e.g. Foo::Bar) '.
+            'which will be searched in @INC',
+        schema => ['array*', of=>'perl::pod_filename*'],
+        description => <<'_',
+
+"-" means standard input.
+
+_
+        pos => 0,
+        default => ['-'],
+        slurpy => 1,
+    },
+);
+
+$SPEC{dump_pod_structure} = {
+    v => 1.1,
+    summary => 'Dump POD structure using Pod::Elemental',
+    description => <<'_',
+
+This is actually just a shortcut for:
+
+    % podsel FILENAME --root --dump --transform Pod5 --transform Nester
+
+_
+    args => {
+        %arg0_pod,
+    },
+    links => [
+        {url=>'prog:pomdump', summary=>'Similar script, but using Pod::POM as backend to parse POD document into tree'},
+        {url=>'prog:podsel'},
+    ],
+};
+sub dump_pod_structure {
+    require App::podsel;
+
+    my %args = @_;
+
+    App::podsel::podsel(
+        file => $args{pod},
+        select_action => "root",
+        node_actions => ['dump'],
+        transforms => ['Pod5', 'Nester'],
+    );
+}
 
 1;
 # ABSTRACT: Command-line utilities related to POD
